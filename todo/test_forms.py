3daa803ad69e7b46e00770460c81fa58d0d35b8f
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-14 09:38:21 +0000

    Completed testing forms

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
+from .forms import ItemForm
 
 # Create your tests here.
-class TestDjango(TestCase):
+class TestToDoItemForm(TestCase):
     
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+    def test_can_create_an_item_with_just_a_name(self):
+        form = ItemForm({'name': 'Create Tests'})
+        self.assertTrue(form.is_valid())
+
+    def test_correct_message_for_missing_name(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
+
