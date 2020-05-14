06dc1d988e801ac65a2ddb0d09efc58f85f5bb94
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-14 10:29:27 +0000

    Added htmlcov folder and installed coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -15,3 +15,7 @@ class TestItemModel(TestCase):
         item.save()
         self.assertEqual(item.name, "Create a Test")
         self.assertTrue(item.done)
+
+    def test_item_as_a_string(self):
+        item = Item(name="Create a Test")
+        self.assertEqual("Create a Test", str(item))
