6236b8004cf9a69ba07f1d9557e3fbf5c9e194fa
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-14 08:42:47 +0000

    finished editing an item

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import render, HttpResponse, redirect, get_object_or_404
 from .models import Item
 from .forms import ItemForm
 
@@ -16,4 +16,15 @@ def create_an_item(request):
     else: 
         form = ItemForm()
 
+    return render(request, "item_form.html", {'form': form})
+
+def edit_an_item(request, id):
+    item = get_object_or_404(Item, pk=id)
+    if request.method == "POST":
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect(get_todo_list)
+    else:
+        form = ItemForm(instance=item)
     return render(request, "item_form.html", {'form': form})
