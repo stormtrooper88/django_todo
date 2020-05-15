b4edc00179ba772044a849349ad599535df963dc
Thomas Christian Butcher <thomas.c.butcher@gmail.com>
2020-05-15 08:14:06 +0000

    prep settings and requirements for deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,12 +75,14 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#   'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
+DATABASES = {'default': dj_database_url.parse("postgres://wkopxgsoxsinek:7acbc1f9dcae33f3e3c0218c9d73bc7ad455a4df8cdd634bfdd94b0bcc540eb8@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d7t5ghp90pec30")}
 
 
 # Password validation
